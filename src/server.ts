719efccddc6a08bed148fee0bf872f8de035e627
Volkova Annie <annie_v@bk.ru>
2019-01-21 23:46:54 +0300

    Add chat

--- src/server.ts
+++ src/server.ts
@@ -9,22 +9,38 @@ const server = http.createServer(app);
 
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
+let counter = 0;
+const messages = new Array();
 
 wss.on('connection', (ws: WebSocket) => {
 
+  const sendMessages = () => {
+    const messageToSend = messages.join('\n');
+    ws.send(messageToSend);
+  }
+  const userName = `person_${counter++}`;
+
     //connection is up, let's add a simple simple event
     ws.on('message', (message: string) => {
-
+        messages.push(`${userName}: ${message}`);
         //log the received message and send it back to the client
         console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+        console.log('messages', messages);
+        sendMessages();
     });
 
+    setInterval(() => {
+      sendMessages();
+    }, 1000);
+
     //send immediatly a feedback to the incoming connection
-    ws.send('Hi there, I am a WebSocket server');
+    sendMessages();
+
+
 });
 
 //start our server
 server.listen(process.env.PORT || 8999, () => {
-    console.log(`Server started on port ${server.address().port} :)`);
+    // console.log(`Server started on port ${server.address().port} :)`);
+    console.log(`Server started on port 8999`);
 });
